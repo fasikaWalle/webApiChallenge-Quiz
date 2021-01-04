1f280ec2bdc5a991d140068bbb970bc11655fd1a
fasikaWalle <fasikabini12@gmail.com>
2021-01-03 20:02:41 -0600

    sorting and displaying the user scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -102,21 +102,27 @@ function saveUserScore(event) {
   var userNameInput = document.querySelector("input[name='userName']").value;
   userNameInput = userNameInput.toLowerCase();
   var userScoreInfo = { userName: userNameInput, score: score };
-  userScore = localStorage.getItem("score") || [];
-  if (userScore.length > 0) {
-    userScore = JSON.parse(userScore);
-  }
+  var userScore = getUserScore();
+  console.log(userScore);
+  // if (userScore.length > 0) {
+  //   userScore = JSON.parse(userScore);
+  // }
   var userNameExist = userValidation(userScore, userNameInput);
   if (userNameExist) {
     event.preventDefault();
     userValid.setAttribute("style", "display:block");
     userInfo.append(userValid);
   } else {
+    console.log(userScore);
     userScore.push(userScoreInfo);
-    userScore = JSON.stringify(userScore);
-    localStorage.setItem("score", userScore);
+    LocalStorageSetItem(userScore);
     fetchHighScore();
   }
+}
+
+function LocalStorageSetItem(userScore) {
+  userScore = JSON.stringify(userScore);
+  localStorage.setItem("score", userScore);
 }
 //user validation which checks if user name already exist
 function userValidation(userScore, userNameInput) {
@@ -127,27 +133,55 @@ function userValidation(userScore, userNameInput) {
   }
   return false;
 }
+//function which used to get the local storage array
+function getUserScore() {
+  var userScore = localStorage.getItem("score") || [];
+  if (userScore.length > 0) {
+    userScore = JSON.parse(userScore);
+  }
+  return userScore;
+}
+//function which sort the localstorage array
+function sortArray(highScore) {
+  highScore.sort(function (firstScore, secondScore) {
+    return secondScore.score - firstScore.score;
+  });
+}
 //retrive the stored data from local storage
 function fetchHighScore() {
   clearDiv();
+  var i = 0;
   userValid.setAttribute("style", "display:none");
   checkeAnswer.setAttribute("style", "display:none");
-  var highScore = localStorage.getItem("score");
-  highScore = JSON.parse(highScore);
+  var userScore = getUserScore();
   divContainer.removeChild(timeContainer);
   scoreDiv.setAttribute("style", "display:block");
   divContainer.append(scoreDiv);
-  var max = -1;
-  var highScoreUser;
-  for (var i = 0; i < highScore.length; i++) {
-    var score = highScore[i].score;
-    if (max < score) {
-      max = score;
-      highScoreUser = highScore[i];
+  if (userScore.length < 0) {
+    return;
+  }
+  var i = 0;
+  var scoreDisplayBox = document.getElementById("scoreDisplay");
+  //check whether the array in the localstorage is empty or not
+  if (userScore.length < 0) {
+    return;
+  }
+  //to sort the user information based on their score
+  sortArray(userScore);
+  var UserscoresList = document.createElement("ol");
+  while (userScore.length > i) {
+    var userScoreInfo =
+      userScore[i].userName + "---------------" + userScore[i].score;
+    var scoreListItems = document.createElement("li");
+    scoreListItems.setAttribute("style", "padding-bottom:20px");
+    UserscoresList.append(scoreListItems);
+    scoreListItems.innerHTML = userScoreInfo;
+    scoreDisplayBox.append(UserscoresList);
+    i++;
+    if (i == 4) {
+      break;
     }
   }
-  document.getElementById("scoreDisplay").innerHTML =
-    "1. " + highScoreUser.userName + " - " + highScoreUser.score;
 }
 
 //display final score of the user
@@ -159,26 +193,19 @@ function displayScore() {
 }
 //restart the quiz
 function restartTheQuiz() {
-  // window.location.href = "https://fasikawalle.github.io/webApiChallenge-Quiz/";
+  window.location.href = "https://fasikawalle.github.io/webApiChallenge-Quiz/";
 }
 //clear high score by making the paragraph content empty
 function clearHighScore() {
-  document.getElementById("scoreDisplay").innerHTML = "";
-  var highScore = localStorage.getItem("score");
-  console.log(highScore);
-  highScore = JSON.parse(highScore);
-  var max = 0;
-  var indexOfHihScoreObj = 0;
-  for (var i = 0; i < highScore.length; i++) {
-    var score = highScore[i].score;
-    if (max < score) {
-      max = score;
-      indexOfHihScoreObj = i;
-    }
+  var highScore = getUserScore();
+  if (highScore.length < 0) {
+    return;
   }
-  highScore.splice(indexOfHihScoreObj, 1);
-  console.log(highScore);
-  localStorage.setItem("score", JSON.stringify(highScore));
+  sortArray(highScore);
+  highScore.shift();
+  LocalStorageSetItem(highScore);
+  document.getElementById("scoreDisplay").innerHTML = "";
+  fetchHighScore();
 }
 startButton.addEventListener("click", startTheQuiz);
 btnRestart.addEventListener("click", restartTheQuiz);
